e1f05377fa3d64ebf18e599384abbd36ac00e9b4
Samet Arslan <86328409+samarslan@users.noreply.github.com>
2021-12-30 06:48:46 +0000

    Hesaplama

---
+++ 30.12.2021hesaplama.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+main(){
+  int a, b, c, sec;
+
+  printf("a sayisi:");
+  scanf("%d", &a);
+  printf("b sayisi:");
+  scanf("%d", &b);
+
+  printf("1-Toplama: ");
+  printf("\n2-Çıkarma: ");
+  printf("\n3-Çarpma: ");
+  printf("\n4-Bölme: ");
+  scanf("%d", &sec);
+
+  if(sec==1){
+    
+    c=a+b;
+    printf("toplam: %d",c);
+  }
+  if(sec==2){
+    
+    c=a-b;
+    printf("cevap: %d",c);
+  }
+  if(sec==3){
+    
+    c=a*b;
+    printf("cevap: %d",c);
+  }
+  if(sec==4){
+    
+    c=a/b;
+    printf("cevap: %d",c);
+  }
+  
+}
