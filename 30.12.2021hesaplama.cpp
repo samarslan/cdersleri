7787afa79e1c4855dcfc4987fc64de8ed180be09
Samet Arslan <86328409+samarslan@users.noreply.github.com>
2022-01-01 11:40:21 +0000

    düzenleme

--- 30.12.2021hesaplama.cpp
+++ 30.12.2021hesaplama.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-main(){
+int main(){
   int a, b, c, sec;
 
   printf("a sayisi:");
--- 30.12.2021sayilar.cpp
+++ 30.12.2021sayilar.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-main(){
+int main(){
   int a, b, c, sayi;
 
   printf("yüzler basamağı: ");
