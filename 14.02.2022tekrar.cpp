6f85358289016315abd7d5912ce5b5092c0edbc2
Samet Arslan <86328409+samarslan@users.noreply.github.com>
2022-02-14 07:48:24 +0000

    1. ve 2. tekrar proejeleri eklendi

---
+++ 14.02.2022tekrar.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+  int a, b, c;
+  
+  printf("1.Sayıyı Girin: ");
+  scanf("%d", &a);
+  
+  printf("2.Sayıyı Girin: ");
+  scanf("%d", &b);
+
+  c=a+b;
+
+  printf("toplam: %d", c);
+}
---
+++ 14.02.2022tekrar2.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+
+int main()
+{
+  int secim, a, b, c;
+
+  printf("1-ÜÇGEN ALANI \n2-KARE ALANI \n3-DİKDÖRTGEN ALANI ");
+  printf("\nSeçim Yapın: ");
+  scanf("%d", &secim);
+
+  switch(secim){
+    case 1:{
+      printf("Taban girin: ");
+      scanf("%d", &a);
+
+      printf("Yükseklik girin: ");
+      scanf("%d", &b);
+
+      c=a*b/2;
+      
+      break;
+    }
+    case 2:{
+      printf("Kenarı girin: ");
+      scanf("%d", &a);
+      
+      c=a*a;
+      
+      break;
+    }
+    case 3:{
+      printf("1.Kenarı girin: ");
+      scanf("%d", &a);
+
+      printf("2.Kenarı girin: ");
+      scanf("%d", &b);
+
+      c=a*b;
+      break;
+    }
+    default:{
+      printf("Geçersiz Seçim.");
+    }
+  }
+  printf("%d", c);
+}
