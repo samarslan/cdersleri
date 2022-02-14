f972b0158fb21332e3816736e2177d021b9f067d
Samet Arslan <86328409+samarslan@users.noreply.github.com>
2022-02-14 08:48:04 +0000

    3.tekrar projesi eklendi

---
+++ 14.02.2022tekrar3.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+  int sayi;
+  int b, o, y;
+
+  printf("Sayı Girin: ");
+  scanf("%d", &sayi);
+
+  b = (sayi % 10);
+  o = (((sayi-b) % 100)/10);
+  y = ((sayi-(o*10)-b)/100);
+
+  if(y==b&&o!=y){
+    printf("Mükemmel sayı. ");
+  }
+  else if(y==o&&o!=b){
+    printf("Harika sayı. ");
+  }
+  else if(b==o&&o!=y){
+    printf("Süper sayı. ");
+  }
+  else{
+    printf("Normal sayı. ");
+  }
+}
